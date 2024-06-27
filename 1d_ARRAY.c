9202ee25d69e70675f0772f73b0de902d24bc2a5
Dharshini T <131947889+Sdt320@users.noreply.github.com>
2024-06-27 13:47:59 +0530

    Create 1d_ARRAY.c

---
+++ 1d_ARRAY.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Declare an array of 5 integers
+    int numbers[5];
+
+    // Initialize an array of 5 integers
+    int values[5] = {1, 2, 3, 4, 5};
+
+    // Access elements of the array
+    printf("First element: %d\n", values[0]);
+    printf("Third element: %d\n", values[2]);
+
+    return 0;
+}
